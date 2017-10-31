28d9e4ef9f4025d05d85385a2567aead24354e19
shuchu han <shuchu@nec-labs.com>
2017-10-31 15:36:24 -0400

    updated sample repo

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
 
 
@@ -16,9 +14,9 @@ setup(
     version='0.1.0',
     description='Sample package for Python-Guide.org',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Shuchu Han',
+    author_email='shuchu.han@gmail.com',
+    url='',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
